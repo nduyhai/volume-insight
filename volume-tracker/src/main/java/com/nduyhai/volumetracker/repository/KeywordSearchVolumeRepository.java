0054d6fda0ec7dffcf7614478e8d444ebcea4713
hai.nguyen <15892244+nduyhai@users.noreply.github.com>
2025-01-04 09:47:21 +0700

    feat: add filter by time range

--- volume-tracker/src/main/java/com/nduyhai/volumetracker/repository/KeywordSearchVolumeRepository.java
+++ volume-tracker/src/main/java/com/nduyhai/volumetracker/repository/KeywordSearchVolumeRepository.java
@@ -13,25 +13,26 @@ public interface KeywordSearchVolumeRepository
 
   @Query(
       """
-        SELECT k.name AS name, ksv.createdDatetime AS createdDatetime, ksv.searchVolume AS searchVolume
-          FROM KeywordSearchVolumeEntity ksv
-          JOIN KeywordEntity k ON k.id = ksv.keywordId
-          JOIN UserSubscriptionEntity us ON us.keyword.id = k.id
-          WHERE us.userId = :userId
-            AND k.name IN :keywordNames
-            AND ksv.createdDatetime BETWEEN (
-                SELECT MIN(us.startDatetime)
-                FROM UserSubscriptionEntity us
-                WHERE us.userId = :userId AND us.keyword.id = k.id
-            )
-            AND (
-                SELECT MAX(us.endDatetime)
-                FROM UserSubscriptionEntity us
-                WHERE us.userId = :userId AND us.keyword.id = k.id
-            )
-            AND (us.subscriptionType = 'DAILY'  OR us.subscriptionType = 'HOURLY')
-            AND HOUR(ksv.createdDatetime) = 9
-        ORDER BY ksv.createdDatetime ASC
+          SELECT k.name AS name, ksv.createdDatetime AS createdDatetime, ksv.searchVolume AS searchVolume
+            FROM KeywordSearchVolumeEntity ksv
+            JOIN KeywordEntity k ON k.id = ksv.keywordId
+            JOIN UserSubscriptionEntity us ON us.keyword.id = k.id
+            WHERE us.userId = :userId
+              AND k.name IN :keywordNames
+              AND ksv.createdDatetime BETWEEN (
+                  SELECT MIN(us.startDatetime)
+                  FROM UserSubscriptionEntity us
+                  WHERE us.userId = :userId AND us.keyword.id = k.id
+              )
+              AND (
+                  SELECT MAX(us.endDatetime)
+                  FROM UserSubscriptionEntity us
+                  WHERE us.userId = :userId AND us.keyword.id = k.id
+              )
+               AND ksv.createdDatetime BETWEEN :startTime AND :endTime
+              AND (us.subscriptionType = 'DAILY'  OR us.subscriptionType = 'HOURLY')
+              AND HOUR(ksv.createdDatetime) = 9
+          ORDER BY ksv.createdDatetime ASC
           """)
   List<KeywordVolumeProjection> findDailyDataWithKeyword(
       @Param("userId") String userId,
@@ -57,6 +58,7 @@ public interface KeywordSearchVolumeRepository
                   FROM UserSubscriptionEntity us
                   WHERE us.userId = :userId AND us.keyword.id = k.id
               )
+               AND ksv.createdDatetime BETWEEN :startTime AND :endTime
               AND us.subscriptionType = 'HOURLY'
            ORDER BY ksv.createdDatetime ASC
           """)
